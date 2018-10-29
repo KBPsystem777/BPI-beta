de27ed189dee1e073d8a017dd86b2bf00690d001
kbpsystem777 <koleen.bp@outlook.com>
2018-10-30 00:52:23 +0800

    initial style for login-form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import styled from 'styled-components';
 import LoginForm from './components/LoginForm';
 import Banner from './components/Home';
+import NavigationBar from './components/Nav';
 
 
-
+const LoginFormStyles = styled.div `
+  padding-top: 5rem;
+  padding-left: 10rem;
+`
 class App extends Component {
   render() {
     return (
       <div>
-          <LoginForm />
+          <NavigationBar />
+          <LoginFormStyles>
+            <LoginForm />
+          </LoginFormStyles>
           <Banner />
       </div>
     )
