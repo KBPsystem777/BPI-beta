712e5b72b20030c3cdaebde758599b55b216bb8d
kbpsystem777 <koleen.bp@outlook.com>
2018-10-30 00:53:12 +0800

    UI styles for loginform

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -9,6 +9,25 @@ const Button = styled.button `
     color: white;
 `;
 
+
+const FormStatement = styled.h3 `
+    color: #000;    
+`
+
+const FormUI = styled.form `
+    background-color: #F8F8F8;
+    clear: both;
+    padding: 20px 30px 0;
+    box-sizing: border-box;
+    display: block;
+    line-height: 1.34;
+    color: #393939;
+    width: 35rem;
+    height: 20rem;
+    border-radius: 15px;
+    opacity: 10;
+`
+
 class LoginForm extends React.Component {
     
     onSubmit(e) {
@@ -18,8 +37,9 @@ class LoginForm extends React.Component {
     onSubmit = this.onSubmit.bind(this);
     render() {
         return(
-            <div>
-                <form onSubmit={this.onSubmit}>
+            <div className="FormControl">
+                <FormUI onSubmit={this.onSubmit}>
+                    <FormStatement>Login to your account</FormStatement>
                     <div>
                         <p>Username</p>
                         <input type="text" />
@@ -29,7 +49,7 @@ class LoginForm extends React.Component {
                         <input type="password" required />
                     </div>
                     <Button>Login</Button>
-                </form>
+                </FormUI>
             </div>
         )
     }
